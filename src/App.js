30ef6f755e7c48578ea472e02fa184a8a806e10f
Ahmad Nuril Firdaus <firdausnurilahmad@gmail.com>
2020-12-31 13:18:03 +0700

    fix image

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { makeStyles } from "@material-ui/core/styles";
 import { CssBaseline } from "@material-ui/core";
 import Header from "./components/Header";
 import PlaceToVisit from "./components/PlaceToVisit";
+import bghome from "./assets/bg.jpg";
 
 const useStyles = makeStyles((themes) => ({
   root: {
     minHeight: "100vh",
-    backgroundImage: "url('/assets/bg.jpg')",
+    backgroundImage: `url(${bghome})`,
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
   },
