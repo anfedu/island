2541e25d2a3844d07155a77d652978d9ab6ea696
Ahmad Nuril Firdaus <firdausnurilahmad@gmail.com>
2020-12-31 13:25:40 +0700

    fix image

--- src/static/Places.js
+++ src/static/Places.js
@@ -1,16 +1,19 @@
+import island1 from "../assets/island1.jpg";
+import island2 from "../assets/island2.jpg";
+
 const places = [
   {
     title: "Green",
     description:
       "The Madives are home to some of the world's most ravishing islands, but it's veri wonderful place",
-    imageUrl: "/assets/island1.jpg",
+    imageUrl: island1,
     time: 1500,
   },
   {
     title: "Bora Bora",
     description:
       "Shaped like a giant sombrero, this lish volcanic island stars in countless Soon",
-    imageUrl: "/assets/island2.jpg",
+    imageUrl: island2,
     time: 1500,
   },
 ];
